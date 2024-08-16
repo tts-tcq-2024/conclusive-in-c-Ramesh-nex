49777c24cda3336fd92feb844b83d968b80b0b7e
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-16 20:30:43 +0530

    Update and rename Send_Controller.cpp to SendtoController.cpp

--- SendtoController.cpp
+++ SendtoController.cpp
@@ -1,5 +1,5 @@
 #include "BreachType.h"
-// Function to simulate sending an alert to the controller
+
 void sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
