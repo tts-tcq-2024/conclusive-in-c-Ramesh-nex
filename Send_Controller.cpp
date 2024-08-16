1e52cfa508a1561f9504efb6a342cb968750f4f7
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-16 20:28:01 +0530

    Update Send_Controller.cpp

--- Send_Controller.cpp
+++ Send_Controller.cpp
@@ -1,1 +1,6 @@
 
+// Function to simulate sending an alert to the controller
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  printf("%x : %x\n", header, breachType);
+}
