d0ef1c660a7d72838fd3a55f90b949f04f7f0912
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-16 20:47:20 +0530

    Update and rename GetLimit_Breach.cpp to InferBreach.cpp

---
+++ InferBreach.cpp
@@ -0,0 +1,9 @@
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+  if(value < lowerLimit) {
+    return TOO_LOW;
+  }
+  if(value > upperLimit) {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
