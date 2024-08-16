adb3f3119ac355fd52ed1262bb32652a885dfc8f
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-16 20:31:33 +0530

    Update and rename Send_Email.cpp to SendtoEmail.cpp

--- SendtoEmail.cpp
+++ SendtoEmail.cpp
@@ -1,12 +1,15 @@
+#include "BreachType.h"
 
-void sendToEmail(BreachType breachType) {
+void sendToEmail(BreachType breachType)
+{
   const char* recipient = "a.b@c.com";
-
-  // Prepare and print the message based on the breach type
-  if (breachType == TOO_LOW) {
+  if (breachType == TOO_LOW) 
+  {
     printf("To: %s\n", recipient);
     printf("Hi, the temperature is too low\n");
-  } else if (breachType == TOO_HIGH) {
+  }
+  else if (breachType == TOO_HIGH) 
+  {
     printf("To: %s\n", recipient);
     printf("Hi, the temperature is too high\n");
   }
