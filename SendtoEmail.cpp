240f76138ec7a562bca42b425c2aa5261e735c71
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-25 20:01:25 +0530

    Update SendtoEmail.cpp

--- SendtoEmail.cpp
+++ SendtoEmail.cpp
@@ -1,1 +1,15 @@
+void sendToEmail(BreachType breachType)
+{
+  const char* recipient = "a.b@c.com";
+  if (breachType == TOO_LOW) 
+  {
+    printf("To: %s\n", recipient);
+    printf("Hi, the temperature is too low\n");
+  }
+  else if (breachType == TOO_HIGH) 
+  {
+    printf("To: %s\n", recipient);
+    printf("Hi, the temperature is too high\n");
+  }
+}
 
