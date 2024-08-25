f77da76d3925a732484ffbcdd76cf7aa1df640c8
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-25 20:58:45 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -58,17 +58,4 @@ void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double
     }
 }
 
-void defaultSendToEmail(BreachType breachType)
-{
-    const char* recipient = "a.b@c.com";
-    if (breachType == TOO_LOW) 
-    {
-        printf("To: %s\n", recipient);
-        printf("Hi, the temperature is too low\n");
-    }
-    else if (breachType == TOO_HIGH) 
-    {
-        printf("To: %s\n", recipient);
-        printf("Hi, the temperature is too high\n");
-    }
-}
+
